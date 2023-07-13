f1b03da42ee6a0c0134dc7a30ab999266a1f718e
MarkLee131 <1512543916@qq.com>
2023-07-14 00:07:14 +0800

    update tfidf and bm 25 scripts and results

--- TF-IDF/Baseline_TF-IDF.py
+++ TF-IDF/Baseline_TF-IDF.py
@@ -1,4 +1,5 @@
-### Used to calculate BM25 similarity between query and document first
+### Used to calculate TF-IDF similarity between query and document first
+# TF-IDF similarity
 import os
 from tqdm import tqdm
 import pandas as pd
@@ -6,16 +7,16 @@ import pandas as pd
 DATA_DIR = '/data/kaixuan/ramdisk/data'
 DATA_TMP_DIR = '/data/kaixuan/data_tmp'
 
-### read bm25 similarity data
-bm25_data = pd.read_csv(os.path.join(DATA_DIR, 'similarity_data_bm25.csv'))
-bm25_cve = bm25_data.groupby('cve')
+### read tfidf similarity data
+tfidf_data = pd.read_csv(os.path.join(DATA_DIR, 'similarity_data.csv'))
+tfidf_cve = tfidf_data.groupby('cve')
 
 ### calculate the average rank of patch commit for each cve
 ### by using label to determine whether the commit is a patch commit
 rank_info = pd.DataFrame(columns=['cve', 'rank'])
-rank_info.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'), index=False)
+rank_info.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_TDIDF.csv'), index=False)
 
-for cve, group in tqdm(bm25_cve):
+for cve, group in tqdm(tfidf_cve):
     # first sort the rows according to the similarity score
     group = group.sort_values(by='similarity', ascending=False)
     average_rank = 0
@@ -28,15 +29,15 @@ for cve, group in tqdm(bm25_cve):
         ranks.append(rank)
     average_rank = sum(ranks) / len(ranks)
     rank_info_iter = pd.DataFrame([[cve, average_rank]], columns=['cve', 'rank'])
-    rank_info_iter.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'), mode='a', header=False, index=False)
+    rank_info_iter.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_TDIDF.csv'), mode='a', header=False, index=False)
     
 ### calculate the Top@K recall by using the rank info
-def recall(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'recall_BM25.csv')):
+def recall(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'recall_TFIDF.csv')):
     recall_info = pd.DataFrame(columns=['Top@k', 'recall'])
     recall_info.to_csv(save_path, index=False)
 
     for k in k_list:
-        rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'))
+        rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_TDIDF.csv'))
         rank_info['recall'] = rank_info['rank'].apply(lambda x: 1 if x <= k else 0)
         recall = rank_info['recall'].sum() / len(rank_info)
         print('Top@{} recall: {}'.format(k, recall))
@@ -45,8 +46,8 @@ def recall(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_D
         print('Top@{} recall info saved'.format(k))
 
 ### calculate the MRR
-def mrr(rank_info, save_path=os.path.join(DATA_TMP_DIR, 'MRR_BM25.csv')):
-    rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'))
+def mrr(rank_info, save_path=os.path.join(DATA_TMP_DIR, 'MRR_TFIDF.csv')):
+    rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_TDIDF.csv'))
     rank_info['reciprocal_rank'] = rank_info['rank'].apply(lambda x: 1 / x)
     mrr = rank_info['reciprocal_rank'].sum() / len(rank_info)
     print('MRR: {}'.format(mrr))
@@ -55,16 +56,35 @@ def mrr(rank_info, save_path=os.path.join(DATA_TMP_DIR, 'MRR_BM25.csv')):
     print('MRR info saved')
     
 ### calculate the average manual efforts for Top@K
-def manual_efforts(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'manualefforts_BM25.csv')):
+def manual_efforts(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'manualefforts_TFIDF.csv')):
     manual_efforts_info = pd.DataFrame(columns=['Top@k', 'manual_efforts'])
     manual_efforts_info.to_csv(save_path, index=False)
     
     for k in k_list:
-        rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'))
+        rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_TDIDF.csv'))
         rank_info['manual_efforts'] = rank_info['rank'].apply(lambda x: 1 if x <= k else 0)
         manual_efforts = rank_info['manual_efforts'].sum() / len(rank_info)
         print('Top@{} manual efforts: {}'.format(k, manual_efforts))
         manual_efforts_info_iter = pd.DataFrame([[k, manual_efforts]], columns=['Top@k', 'manual_efforts'])
         manual_efforts_info_iter.to_csv(save_path, mode='a', header=False, index=False)
 
+
+
 recall()
+    
+'''
+Top@1 recall: 0.35477135101273755
+Top@1 recall info saved
+Top@5 recall: 0.5080392566297766
+Top@5 recall info saved
+Top@10 recall: 0.5625391522238463
+Top@10 recall info saved
+Top@20 recall: 0.6304030068907914
+Top@20 recall info saved
+Top@30 recall: 0.6652745875965755
+Top@30 recall info saved
+Top@50 recall: 0.7107955731885571
+Top@50 recall info saved
+Top@100 recall: 0.7657130925036542
+Top@100 recall info saved
+'''
--- TF-IDF/TF-IDF_similarity.py
+++ TF-IDF/TF-IDF_similarity.py
@@ -2,52 +2,71 @@
 # import numpy as np
 import os
 import pandas as pd
-from rank_bm25 import BM25Okapi
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from utils import reduce_mem_usage
 from tqdm import tqdm
 import gc
 import multiprocessing as mp
 from tqdm import tqdm
+# nltk.download('punkt')
 
-##### 2023.07.13
-'''
-We replace TF-IDF by using BM25 to calculate the similarity 
-between the description of cve and the commit message of commit.
-'''
+##### 2023.07.07 
+##### This script is revised from TFIDF_similarity_old.py
+##### We changed the logic to first save the tokenized data to csv files, and then load them to save memory.
+
+##### 2023.07.09
+##### We try to paralellize the process of calculating similarity scores, since the single process is too slow (17hs).
 
 DATA_DIR = '/data/kaixuan/ramdisk/data'
 DATA_TMP_DIR = '/data/kaixuan/data_tmp'
 
+# scp -P 32908 kaixuan_cuda11@XXXXXX:/mnt/local/Baselines_Bugs/CodeBert/data/cve_desc.csv ./data/
+
+# sudo mkdir -p /data/kaixuan/ramdisk
+# sudo mount -t tmpfs -o size=110G tmpfs /data/kaixuan/ramdisk
+# mv ./data /data/kaixuan/ramdisk/
+
+# sync
+# sudo umount /data/kaixuan/ramdisk
+
+
+
 def compute_similarity(args):
+    vectorizer = TfidfVectorizer()
     group, cve = args
 
-    corpus = [text.lower().split() for text in group['combined'].fillna('').tolist()]  # Ensure no NaN values in corpus, convert to list of tokens
-    bm25 = BM25Okapi(corpus)
+    try:
+        vectorizer.fit(group['combined'])
+    except Exception as e:
+        group['combined'].fillna('', inplace=True)
+        vectorizer.fit(group['combined'])
 
-    query = group['desc_token'].iloc[0]
-    scores = bm25.get_scores(query.split())  # Tokenize query, BM25 assumes tokenized input
-    # top_50 = bm25.get_top_n(query.split(), corpus, n=50)
+    similarity_scores = []
+    for _, row in group.iterrows():
+        desc_tfidf = vectorizer.transform([row['desc_token']])
+        combined_tfidf = vectorizer.transform([row['combined']])
+        similarity = cosine_similarity(desc_tfidf, combined_tfidf).diagonal()[0]
+        similarity_scores.append(similarity)
 
     similarity_data = pd.DataFrame()
     similarity_data['cve'] = group['cve']
     similarity_data['owner'] = group['owner']
     similarity_data['repo'] = group['repo']
     similarity_data['commit_id'] = group['commit_id']
-    similarity_data['similarity'] = scores
+    similarity_data['similarity'] = similarity_scores
     similarity_data['label'] = group['label']
 
-    # Sort by similarity scores
-    similarity_data = similarity_data.sort_values('similarity', ascending=False)
-
     # Append to CSV
-    similarity_data.to_csv(os.path.join(DATA_DIR, 'similarity_data_bm25.csv'), mode='a', header=False, index=False)
+    similarity_data.to_csv(os.path.join(DATA_DIR, 'similarity_data_1.csv'), mode='a', header=False, index=False)
 
 
 if __name__ == '__main__':
     
     # Create and write the header of the CSV file
     empty_df = pd.DataFrame(columns=['cve', 'owner', 'repo', 'commit_id', 'similarity', 'label'])
-    empty_df.to_csv(os.path.join(DATA_DIR, 'similarity_data_bm25.csv'), index=False)
+    empty_df.to_csv(os.path.join(DATA_DIR, 'similarity_data.csv'), index=False)
+
 
     # Load data
     # commit_data = pd.read_csv(os.path.join(DATA_DIR, 'commit_sample.csv')) ### for test
@@ -70,20 +89,24 @@ if __name__ == '__main__':
     print("Loading tokenized data...")
     desc_df = pd.read_csv(os.path.join(DATA_TMP_DIR, 'desc_token.csv'))
     reduce_mem_usage(desc_df)
-    
-    # replace NaN values with a space, and convert to lowercase
-    desc_df['desc_token'] = desc_df['desc_token'].fillna(' ').str.lower()
+    desc_df['desc_token'] = desc_df['desc_token'].fillna(' ') # replace NaN values with a space
     
     msg_df = pd.read_csv(os.path.join(DATA_TMP_DIR, 'msg_token.csv'))
     reduce_mem_usage(msg_df)
-    
-    msg_df['msg_token'] = msg_df['msg_token'].fillna(' ').str.lower() # replace NaN values with a space
+    msg_df['msg_token'] = msg_df['msg_token'].fillna(' ') # replace NaN values with a space
     
     diff_df = pd.read_csv(os.path.join(DATA_TMP_DIR, 'diff_token.csv'))
     reduce_mem_usage(diff_df)
-    diff_df['diff_token'] = diff_df['diff_token'].fillna(' ').str.lower() # replace NaN values with a space
+    diff_df['diff_token'] = diff_df['diff_token'].fillna(' ') # replace NaN values with a space
+
+    # print("shape of desc_df: ", desc_df.shape)
+    # print("shape of msg_df: ", msg_df.shape)
+    # print("shape of diff_df: ", diff_df.shape)
+
+    ### concat the tokenized data
 
     # Combine tokenized commit messages and diffs
+
     data['combined'] = msg_df['msg_token'] + " " + diff_df['diff_token']
     print("shape of data: ", data.shape)
     print("data['combined'][0]: ", data['combined'][0])
@@ -98,19 +121,11 @@ if __name__ == '__main__':
     pool = mp.Pool(mp.cpu_count())
     
     data_cve = data.groupby('cve')
-    #### calculate the groups of cve
-    len_group = len(data_cve)
     cve_list = data['cve'].unique()
     print("len(cve_list): ", len(cve_list))
-
-    # Process each chunk independently
-    print("Computing BM25 vectors...")
-    # results = list(tqdm(pool.imap_unordered(compute_similarity, [(group, cve) for cve, group in data_cve]), total=len_group, desc="Computing similarity scores"))
-    for _ in tqdm(pool.imap_unordered(compute_similarity, [(group, cve) for cve, group in data_cve]), total=len_group, desc="Computing similarity scores"):
-        pass
-
-    pool.close()
-    pool.join()
-
     
-    print("Saved similarity_data_bm25.csv to {}".format(os.path.join(DATA_DIR, 'similarity_data_bm25.csv')))
+    # Process each chunk independently
+    print("Computing TF-IDF vectors...")
+    results = list(tqdm(pool.imap_unordered(compute_similarity, [(group, cve) for cve, group in data_cve]), total=len(cve_list), desc="Computing similarity scores"))
+    
+    print("Saved similarity_data.csv to {}".format(os.path.join(DATA_DIR, 'similarity_data.csv')))
