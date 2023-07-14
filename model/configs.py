5a6b666de8f125da545fdfa748861149c101f01a
MarkLee131 <1512543916@qq.com>
2023-07-14 17:23:53 +0000

    add some pinrtable info in main.py and add timestamp function for config

--- model/configs.py
+++ model/configs.py
@@ -1,15 +1,27 @@
 import torch
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3,2'
-gpus = [0,1]
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+gpus = [0,1,2,3]
 data_path='/mnt/local/Predict5/data1'
 train_file='train.csv'
 valid_file='valid.csv'
 test_file='test.csv'
-batch_size=64
+batch_size=128
 save_path='/mnt/local/Predict5/output7'
 debug=False
 device = torch.device("cuda" if torch.cuda.is_available() and not debug else 'cpu')
-save_dataset=True #### whether the dataset is ready
+save_dataset=False #### whether the dataset is ready
 alpha=0.05
 gamma = 2
+
+
+import pytz
+from datetime import datetime
+
+### we define a function to get singapore time
+def get_singapore_time():
+    singaporeTz = pytz.timezone("Asia/Singapore") 
+    timeInSingapore = datetime.now(singaporeTz)
+    currentTimeInSinapore = timeInSingapore.strftime("%H:%M:%S")
+    # print("Current time in Singapore is: ", currentTimeInSinapore)
+    print(currentTimeInSinapore)
--- model/main.py
+++ model/main.py
@@ -10,13 +10,21 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 torch.manual_seed(3407)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '3,2'
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
+
+
+
+
+
 if __name__ == '__main__':
-    logging.info('start contruct data iterator')
+
+    configs.get_singapore_time()
+    logging.info('1/4: start to contruct the data iterator.')
     train_iterator, vaild_iterator, test_iterator = prepare_data_feature1.prepare_data()
     
-    logging.info('start contruct model')
-    
+    configs.get_singapore_time()
+    logging.info('2/4: start to contruct our model.')
     model = models.LSTMwithLSTM11(desc_dim=100002, desc_embedding_dim=512, desc_hidden_dim=512, desc_n_layers=1, 
                  code_dim=100002, code_embedding_dim=512, code_hidden_dim=512, code_n_layers=1,
                  dropout=0., hidden_dim=512, output_dim=1)
@@ -26,10 +34,15 @@ if __name__ == '__main__':
     if not configs.debug:
         model.cuda()
         model = torch.nn.DataParallel(model.cuda(), device_ids=configs.gpus, output_device=configs.gpus[0])
-    model.to(configs.device)                                       
-    logging.info('start train')
+    model.to(configs.device)
+    
+    configs.get_singapore_time()                                  
+    logging.info('3/4: start to train.')
     train.train(model=model, train_iterator=train_iterator, vaild_iterator=vaild_iterator, optimizer=optimizer,criterion=criterion,scheduler=scheduler, num_epochs=35, eval_every=1000,save_every=50000, 
                 file_path=configs.save_path, best_valid_loss=float('Inf'))
-    logging.info('start evaluate')
+    
+    configs.get_singapore_time()
+    logging.info('4/4: start to evaluate')
     train.evaluate(model=model, test_iterator=test_iterator,reload_from_checkpoint=True,
                 load_path_checkpoint=os.path.join(configs.save_path,'model_f1.pt'),optimizer=optimizer)
+    configs.get_singapore_time()
