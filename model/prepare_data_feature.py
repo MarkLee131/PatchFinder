b2c707e2685da57bca1e910dd9f24f2a6aa4ceac
MarkLee131 <1512543916@qq.com>
2023-07-17 07:27:05 +0000

    update prepare_data_feature, adapting it to this task

--- model/prepare_data_feature.py
+++ model/prepare_data_feature.py
@@ -62,22 +62,15 @@ def prepare_data():
     MSG = data.Field(tokenize = msg_tokenize,
                     include_lengths=True,batch_first=True)
     
-    FIELD = data.Field(tokenize = desc_tokenize,
+    DESC = data.Field(tokenize = desc_tokenize,
                     include_lengths=True,batch_first=True)
-    
-    
-    #### if the data is some float or int, we use the follows, but in this work, we do not need 
-    # PRIORITY = data.Field(sequential=False, use_vocab=False,dtype=torch.long,batch_first=True)
-    # RANK = data.Field(sequential=False, use_vocab=False,dtype=torch.long,batch_first=True)      
-    # RULE = data.Field(sequential=False, use_vocab=False,dtype=torch.long,batch_first=True)
-    # CAT = data.Field(sequential=False, use_vocab=False,dtype=torch.long,batch_first=True)
     
     
     ##### leave it as default
     LABEL = data.LabelField(dtype = torch.float,batch_first=True)
 
     if not configs.save_dataset:
-        fields = [("diff", DIFF),("Msg", MSG),('Field',FIELD),("Cat", CAT),("Rule",RULE),("Priority",PRIORITY),("Rank",RANK),('FieldWarning',FIELDSWARNING),("Label",LABEL)]
+        fields = [("diff_token", DIFF),("msg_token", MSG),('desc_token', DESC),("label",LABEL)]
         
         #### Caution: take care this field, will load all data.
         train_data, valid_data, test_data = data.TabularDataset.splits(
@@ -97,49 +90,39 @@ def prepare_data():
         MAX_VOCAB_SIZE = 100000
         DIFF.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
         MSG.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
-        FIELDSWARNING.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
-        FIELD.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
+        DESC.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
         LABEL.build_vocab(train_data)
         
         
-        torch.save(DIFF, os.path.join(configs.save_path,'field_code.pth'))
+        torch.save(DIFF, os.path.join(configs.save_path,'field_diff.pth'))
         torch.save(MSG, os.path.join(configs.save_path,'field_msg.pth'))
-        torch.save(RULE, os.path.join(configs.save_path,'field_rule.pth'))
-        torch.save(CAT, os.path.join(configs.save_path,'field_cat.pth'))
-        torch.save(FIELD, os.path.join(configs.save_path,'field_field.pth'))
-        torch.save(FIELDSWARNING, os.path.join(configs.save_path,'field_fieldwarning.pth'))
-        torch.save(PRIORITY, os.path.join(configs.save_path,'field_priority.pth'))
-        torch.save(RANK, os.path.join(configs.save_path,'field_rank.pth'))
+        torch.save(DESC, os.path.join(configs.save_path,'field_desc.pth'))
         torch.save(LABEL, os.path.join(configs.save_path,'field_label.pth'))
+        
         torch.save(train_data.examples, os.path.join(configs.save_path,'train_data.pth'))
         torch.save(valid_data.examples, os.path.join(configs.save_path,'valid_data.pth'))
         torch.save(test_data.examples, os.path.join(configs.save_path,'test_data.pth'))
+        
     else:
 
         train_data_examples = torch.load(os.path.join(configs.save_path,'train_data.pth'))
         valid_data_examples = torch.load(os.path.join(configs.save_path,'valid_data.pth'))
         test_data_examples = torch.load(os.path.join(configs.save_path,'test_data.pth'))
-        DIFF=torch.load(os.path.join(configs.save_path,'field_code.pth'))
+        DIFF=torch.load(os.path.join(configs.save_path,'field_diff.pth'))
         MSG=torch.load(os.path.join(configs.save_path,'field_msg.pth'))
-        RULE=torch.load(os.path.join(configs.save_path,'field_rule.pth'))
-        CAT=torch.load(os.path.join(configs.save_path,'field_cat.pth'))
-        FIELD=torch.load(os.path.join(configs.save_path,'field_field.pth'))
-        FIELDSWARNING=torch.load(os.path.join(configs.save_path,'field_fieldwarning.pth'))
-        PRIORITY=torch.load(os.path.join(configs.save_path,'field_priority.pth'))
-        RANK=torch.load(os.path.join(configs.save_path,'field_rank.pth'))
+        DESC=torch.load(os.path.join(configs.save_path,'field_desc.pth'))
         LABEL=torch.load(os.path.join(configs.save_path,'field_label.pth'))
-        fields = [("Diff", DIFF),("Msg", MSG),('Field',FIELD),("Cat", CAT),("Rule",RULE),("Priority",PRIORITY),("Rank",RANK),('FieldWarning',FIELDSWARNING),("Label",LABEL)]
+        fields = [("Diff", DIFF),("Msg", MSG),('Desc',DESC),("Label",LABEL)]
 
         train_data = CustomDataset(train_data_examples, fields)
         valid_data = CustomDataset(valid_data_examples, fields)
         test_data = CustomDataset(test_data_examples, fields)
         MAX_VOCAB_SIZE = 100000
-        DIFF.build_vocab(train_data.diff,train_data.Field, max_size = MAX_VOCAB_SIZE)
-        MSG.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
+        DIFF.build_vocab(train_data.Diff, max_size = MAX_VOCAB_SIZE)
+        MSG.build_vocab(train_data.Msg, max_size = MAX_VOCAB_SIZE)
         LABEL.build_vocab(train_data)
-        FIELDSWARNING.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
-        FIELD.build_vocab(train_data.diff,train_data.Field, max_size = MAX_VOCAB_SIZE)
-        # FIELD.build_vocab(train_data, max_size = MAX_VOCAB_SIZE)
+        DESC.build_vocab(train_data.Desc, max_size = MAX_VOCAB_SIZE)
+        ###################TODO Ask Han, what meaning for each parameter in build_vocab
         
         
     ##### construct iterator
