fe127354fa6c97d49fa3dd2fa8f97a5152a8bdc2
MarkLee131 <1512543916@qq.com>
2023-08-31 07:39:37 +0000

    update the model (only use CodeReviewer).

---
+++ model/load_data_new.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+class CVEDataset(Dataset):
+    def __init__(self, file_name):
+        self.df = pd.read_csv(file_name)
+        self.cve = self.df['cve']
+        self.desc_tokens = self.df['desc_token']
+        # Combine msg and diff tokens with a space separator
+        self.msg_diff_tokens = self.df['msg_token'] + " " + self.df['diff_token']
+        self.labels = self.df['label']
+        self.tokenizer = AutoTokenizer.from_pretrained("microsoft/codereviewer")
+        
+    def __getitem__(self, index):
+        desc = self.desc_tokens[index] if isinstance(self.desc_tokens[index], str) else ''
+        desc_encoding = self.tokenizer.encode_plus(
+            desc,
+            add_special_tokens=True,
+            max_length=128,
+            return_token_type_ids=False,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True
+        )
+        
+        msg_diff = self.msg_diff_tokens[index] if isinstance(self.msg_diff_tokens[index], str) else ''
+        msg_diff_encoding = self.tokenizer.encode_plus(
+            msg_diff,
+            add_special_tokens=True,
+            max_length=256,  # Increased length as we're concatenating msg and diff
+            return_token_type_ids=False,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True
+        )
+
+        return {
+            'input_ids_desc': desc_encoding['input_ids'].flatten(),
+            'attention_mask_desc': desc_encoding['attention_mask'].flatten(),
+            'input_ids_msg_diff': msg_diff_encoding['input_ids'].flatten(),
+            'attention_mask_msg_diff': msg_diff_encoding['attention_mask'].flatten(),
+            'label': torch.tensor(self.labels[index], dtype=torch.float),
+            'cve': self.cve[index]
+        }
+
+    def __len__(self):
+        return len(self.df)
--- model/main_new.py
+++ model/main_new.py
@@ -9,7 +9,7 @@ As you can see, them model is based on the pytorch-lightning framework.
 
 
 import configs
-from load_data import CVEDataset
+from load_data_new import CVEDataset
 import logging
 from torch.utils.data import DataLoader
 import os
@@ -17,6 +17,8 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
 gpus = [0,1,2,3]
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# set `no_deprecation_warning=True` to disable this warning
 
 
 ####### Load the data loaders
@@ -27,12 +29,12 @@ train_data = CVEDataset(configs.train_file)
 valid_data = CVEDataset(configs.valid_file)
 test_data = CVEDataset(configs.test_file)
 
-train_dataloader = DataLoader(dataset=train_data, shuffle=True, batch_size=32, num_workers=10)
-valid_dataloader = DataLoader(dataset=valid_data, batch_size=32, num_workers=10)
-test_dataloader = DataLoader(test_data, batch_size=4, num_workers=10)
+train_dataloader = DataLoader(dataset=train_data, shuffle=True, batch_size=32, num_workers=15)
+valid_dataloader = DataLoader(dataset=valid_data, batch_size=32, num_workers=15)
+test_dataloader = DataLoader(test_data, batch_size=8, num_workers=15)
 
-# batch = next(iter(train_dataloader))
-# print(batch.keys())
+batch = next(iter(train_dataloader))
+print(batch.keys())
 
 
 """## Fine-tune using PyTorch Lightning
@@ -55,11 +57,8 @@ import torch
 
 class CVEClassifier(pl.LightningModule):
     def __init__(self, 
-                 lstm_hidden_size,
-                 num_classes,
-                 lstm_layers=1,
+                 num_classes=1,
                  dropout=0.1,
-                 lstm_input_size=512,
                  lr=5e-5,
                  num_train_epochs=20,
                  warmup_steps=1000,
@@ -69,56 +68,23 @@ class CVEClassifier(pl.LightningModule):
         self.codeReviewer = AutoModelForSeq2SeqLM.from_pretrained(
             "microsoft/codereviewer").encoder
         
-        ### This is default setting.
-        # # Set requires_grad=True for all parameters of codeReviewer to fine-tune it
-        # for param in self.codeReviewer.parameters():
-        #     param.requires_grad = True
-
-        
         self.save_hyperparameters()
-        
-        # LSTM parameters
-        self.lstm_hidden_size = lstm_hidden_size
-        self.lstm_layers = lstm_layers
         self.dropout = dropout
-        self.lstm_input_size = lstm_input_size
         self.criterion = nn.BCEWithLogitsLoss()
-
-        self.desc_embedding = nn.Embedding(32216, lstm_input_size)
-        '''
-        vocab size: 32216
-        https://huggingface.co/microsoft/codereviewer/blob/main/config.json#L94
-        '''
-
-        # LSTM layer
-        self.lstm = nn.LSTM(input_size=self.lstm_input_size,
-                            hidden_size=self.lstm_hidden_size,
-                            num_layers=self.lstm_layers,
-                            bidirectional=True,
-                            batch_first=True)
 
         # Dropout layer
         self.dropout_layer = nn.Dropout(self.dropout)
-        # Fully connected layer for output, and take care of the bidirectional
-        self.fc = nn.Linear(2 * self.lstm_hidden_size + 2 * self.codeReviewer.config.hidden_size, num_classes)
+        # Fully connected layer for output
+        self.fc = nn.Linear(2 * self.codeReviewer.config.hidden_size, num_classes)
 
-    def forward(self, input_ids_desc, attention_mask_desc, input_ids_msg,
-                attention_mask_msg, input_ids_diff, attention_mask_diff):
+    def forward(self, input_ids_desc, attention_mask_desc, input_ids_msg_diff, attention_mask_msg_diff):
         
-        # Getting embeddings for all inputs
-        desc_embed = self.desc_embedding(input_ids_desc)
+        # Get [CLS] embeddings for desc and msg+diff
+        desc_cls_embed = self.codeReviewer(input_ids=input_ids_desc, attention_mask=attention_mask_desc).last_hidden_state[:, 0, :]
+        msg_diff_cls_embed = self.codeReviewer(input_ids=input_ids_msg_diff, attention_mask=attention_mask_msg_diff).last_hidden_state[:, 0, :]
         
-        # Pass through LSTM and max-pooling
-        lstm_output, _ = self.lstm(desc_embed)#, None)  # Passing None for hidden state and cell state will initialize them with 0s
-        max_pooled, _ = torch.max(lstm_output, 1)  # Max pooling
-        
-        
-        # Get [CLS] embeddings for msg and diff
-        msg_cls_embed = self.codeReviewer(input_ids=input_ids_msg, attention_mask=attention_mask_msg).last_hidden_state[:, 0, :]
-        diff_cls_embed = self.codeReviewer(input_ids=input_ids_diff, attention_mask=attention_mask_diff).last_hidden_state[:, 0, :]
-        
-        # Concatenate max-pooled LSTM output and [CLS] embeddings
-        concatenated = torch.cat((max_pooled, msg_cls_embed, diff_cls_embed), dim=1)
+        # Concatenate [CLS] embeddings
+        concatenated = torch.cat((desc_cls_embed, msg_diff_cls_embed), dim=1)
         
         # Apply dropout
         dropped = self.dropout_layer(concatenated)
@@ -128,36 +94,32 @@ class CVEClassifier(pl.LightningModule):
         
         return output
     
-    def common_step(self, batch, batch_idx):
-        
+    def common_step(self, batch):
         predict = self(
             batch['input_ids_desc'],
             batch['attention_mask_desc'],
-            batch['input_ids_msg'],
-            batch['attention_mask_msg'],
-            batch['input_ids_diff'],
-            batch['attention_mask_diff']
+            batch['input_ids_msg_diff'],  # Updated to msg_diff
+            batch['attention_mask_msg_diff']  # Updated to msg_diff
         )
-        # ValueError: Target size (torch.Size([512])) must be the same as input size (torch.Size([512, 1]))
         predict = predict.squeeze(1)
         loss = self.criterion(predict, batch['label'])
         return loss
 
-    def training_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)
+    def training_step(self, batch, dataloader_idx=None):
+        loss = self.common_step(batch)
         # logs metrics for each training_step,
         # and the average across the epoch
         self.log("training_loss", loss)
         return loss
 
-    def validation_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)
+    def validation_step(self, batch, batch_idx, dataloader_idx=None):
+        loss = self.common_step(batch)
         self.log("validation_loss", loss, on_epoch=True, prog_bar=True, sync_dist=True)
 
         return loss
 
-    def test_step(self, batch, batch_idx):
-        loss = self.common_step(batch, batch_idx)
+    def test_step(self, batch, batch_idx, dataloader_idx=None):
+        loss = self.common_step(batch)
 
         return loss
 
@@ -201,14 +163,8 @@ logging.info('2/4: start to construct our model.')
 
 
 model = CVEClassifier(
-        lstm_hidden_size=256,
         num_classes=1,   # binary classification
-        lstm_layers=1,
-        dropout=0.1,
-        lstm_input_size=512,  # Assuming a 512-sized embedding
-        # lr=5e-5, 
-        # num_train_epochs=20, 
-        # warmup_steps=1000,
+        dropout=0.1
     )
 
 
@@ -218,25 +174,27 @@ from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
 import os
 
 
-wandb_logger = WandbLogger(name='train', project='PatchSleuth')
+wandb_logger = WandbLogger(name='train_0831', project='PatchSleuth')
 # for early stopping, see https://pytorch-lightning.readthedocs.io/en/1.0.0/early_stopping.html?highlight=early%20stopping
 early_stop_callback = EarlyStopping(
     monitor='validation_loss',
-    patience=20,
+    patience=4,
     strict=False,
     verbose=False,
     mode='min'
 )
 lr_monitor = LearningRateMonitor(logging_interval='step')
 
-os.makedirs("/mnt/local/Baselines_Bugs/PatchSleuth/model/output/Checkpoints", exist_ok=True)
+CHECK_POINTS_PATH = "/mnt/local/Baselines_Bugs/PatchSleuth/model/output_0831/Checkpoints"
+
+os.makedirs(CHECK_POINTS_PATH, exist_ok=True)
 
 
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 checkpoint_callback = ModelCheckpoint(
     monitor='validation_loss',
-    dirpath='/mnt/local/Baselines_Bugs/PatchSleuth/model/output/Checkpoints',
+    dirpath=CHECK_POINTS_PATH,
     filename='best-checkpoint',
     save_top_k=1,
     mode='min'
@@ -247,7 +205,7 @@ trainer = Trainer(
     accelerator='gpu',                    # "cpu", "gpu", "tpu", "ipu", "hpu", "mps", "auto", "ddp"
     num_nodes=1,                          # Using 1 machine/node
     devices=len(gpus),                            # Using 4 GPUs
-    default_root_dir="/mnt/local/Baselines_Bugs/PatchSleuth/model/output/Checkpoints",
+    default_root_dir=CHECK_POINTS_PATH,   # Default checkpoint path
     logger=wandb_logger,
     max_epochs=20,                        # Set the maximum number of epochs
     accumulate_grad_batches=1,           # Gradient accumulation steps
@@ -262,9 +220,9 @@ torch.set_float32_matmul_precision("medium")
 trainer.fit(model)
 
 """Once we're done training, we can also save the HuggingFace model as follows:"""
-model_save_path = "/mnt/local/Baselines_Bugs/PatchSleuth/model/output/Checkpoints/final_model.pt"
+model_save_path = os.path.join(CHECK_POINTS_PATH, "final_model.pt")
 
-# torch.save(model.state_dict(), model_save_path)
+
 
 #### So we do not need to load the model Class when evaluate.
 torch.save(model, model_save_path)
