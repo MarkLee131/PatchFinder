ea29417245ee47d308b156699990d3257b91b11c
MarkLee131 <1512543916@qq.com>
2023-09-23 00:38:53 +0800

    update the BM25

--- BM25/BM25.py
+++ BM25/BM25.py
@@ -3,11 +3,11 @@ import os
 from tqdm import tqdm
 import pandas as pd
 
-DATA_DIR = '/data/kaixuan/ramdisk/data'
-DATA_TMP_DIR = '/data/kaixuan/data_tmp'
+DATA_DIR = '/home/kaixuan/patch_locating/PatchSleuth/BM25'
+DATA_TMP_DIR = '/home/kaixuan/patch_locating/PatchSleuth/BM25'
 
 ### read bm25 similarity data
-bm25_data = pd.read_csv(os.path.join(DATA_DIR, 'similarity_data_bm25.csv'))
+bm25_data = pd.read_csv(os.path.join(DATA_DIR, 'similarity_data_bm25okapi.csv'))
 bm25_cve = bm25_data.groupby('cve')
 
 ### calculate the average rank of patch commit for each cve
@@ -17,21 +17,34 @@ rank_info.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'), index=False)
 
 for cve, group in tqdm(bm25_cve):
     # first sort the rows according to the similarity score
-    group = group.sort_values(by='similarity', ascending=False)
+    sorted_group = group.sort_values(by='similarity', ascending=False)
+    
+    # create a mapping from original index to rank
+    rank_mapping = {index: rank+1 for rank, (index, _) in enumerate(sorted_group.iterrows())}
+    
     average_rank = 0
-    #### maybe there are multiple patch commits for one cve
+    # get rows corresponding to patch commits
     patch_rows = group[group['label'] == 1]
     ranks = []
     for _, row in patch_rows.iterrows():
-        ### get the rank by using the index of the row
-        rank = group.index.get_loc(row.name) + 1
+        # get the rank using the rank mapping
+        rank = rank_mapping[row.name]
         ranks.append(rank)
-    average_rank = sum(ranks) / len(ranks)
+        
+    try:
+        average_rank = sum(ranks) / len(ranks)
+    except ZeroDivisionError:
+        print('cve: {} has no patch commit'.format(cve))
+        average_rank = 1
+
+    
     rank_info_iter = pd.DataFrame([[cve, average_rank]], columns=['cve', 'rank'])
     rank_info_iter.to_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'), mode='a', header=False, index=False)
     
 ### calculate the Top@K recall by using the rank info
-def recall(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'recall_BM25.csv')):
+k_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 50, 100]
+
+def recall(k_list=k_list, save_path=os.path.join(DATA_TMP_DIR, 'recall_BM25.csv')):
     recall_info = pd.DataFrame(columns=['Top@k', 'recall'])
     recall_info.to_csv(save_path, index=False)
 
@@ -45,7 +58,7 @@ def recall(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_D
         print('Top@{} recall info saved'.format(k))
 
 ### calculate the MRR
-def mrr(rank_info, save_path=os.path.join(DATA_TMP_DIR, 'MRR_BM25.csv')):
+def mrr(save_path=os.path.join(DATA_TMP_DIR, 'MRR_BM25.csv')):
     rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'))
     rank_info['reciprocal_rank'] = rank_info['rank'].apply(lambda x: 1 / x)
     mrr = rank_info['reciprocal_rank'].sum() / len(rank_info)
@@ -55,17 +68,24 @@ def mrr(rank_info, save_path=os.path.join(DATA_TMP_DIR, 'MRR_BM25.csv')):
     print('MRR info saved')
     
 ### calculate the average manual efforts for Top@K
-def manual_efforts(k_list=[1, 5, 10, 20, 30, 50, 100], save_path=os.path.join(DATA_TMP_DIR, 'manualefforts_BM25.csv')):
+def manual_efforts(k_list=k_list, save_path=os.path.join(DATA_TMP_DIR, 'manualefforts_BM25.csv')):
     manual_efforts_info = pd.DataFrame(columns=['Top@k', 'manual_efforts'])
     manual_efforts_info.to_csv(save_path, index=False)
     
     for k in k_list:
         rank_info = pd.read_csv(os.path.join(DATA_TMP_DIR, 'rankinfo_BM25.csv'))
-        rank_info['manual_efforts'] = rank_info['rank'].apply(lambda x: 1 if x <= k else 0)
+        rank_info['manual_efforts'] = rank_info['rank'].apply(lambda x: 1 if x <= k else k)
         manual_efforts = rank_info['manual_efforts'].sum() / len(rank_info)
         print('Top@{} manual efforts: {}'.format(k, manual_efforts))
         manual_efforts_info_iter = pd.DataFrame([[k, manual_efforts]], columns=['Top@k', 'manual_efforts'])
         manual_efforts_info_iter.to_csv(save_path, mode='a', header=False, index=False)
+
+# def check_scores(file_path=os.path.join(DATA_DIR, 'similarity_data_bm25+1.6_0.3_0.7.csv')):
+#     df = pd.read_csv(file_path)
+#     negative_rows = df[df['similarity'] < 0]
+#     print(negative_rows)
+# check_scores()
+
 
 recall()
 mrr()
