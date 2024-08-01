ecd2c81c2c7fcaf84c90f81dfa54ac4a3042d094
MarkLee13 <1512543916@qq.com>
2024-08-01 20:50:47 +0800

    update overview png

--- BM25/bm25_0920_similarity.py
+++ BM25/bm25_0920_similarity.py
@@ -5,6 +5,7 @@ import gc
 import multiprocessing as mp
 from tqdm import tqdm
 import datetime
+import time
 
 DATA_DIR = '/home/kaixuan/patch_locating/data/split_data'
 SAVE_DIR = '/home/kaixuan/patch_locating/PatchSleuth/BM25'
@@ -13,23 +14,27 @@ train_file = os.path.join(DATA_DIR, 'train_data.csv')
 test_file = os.path.join(DATA_DIR, 'test_data.csv')
 validate_file = os.path.join(DATA_DIR, 'validate_data.csv')
 
-def filter_large_groups(group):
-    if len(group) > 5000:
-        positive_rows = group[group['label'] == 1]
-        negative_rows = group[group['label'] == 0].sample(n=4999)
-        return pd.concat([positive_rows, negative_rows])
-    return group
+# def filter_large_groups(group):
+#     if len(group) > 5000:
+#         positive_rows = group[group['label'] == 1]
+#         negative_rows = group[group['label'] == 0].sample(n=4999)
+#         return pd.concat([positive_rows, negative_rows])
+#     return group
 
 def compute_similarity(args):
+    duration = []
     group, cve = args
-    group = filter_large_groups(group)
+    # group = filter_large_groups(group)
 
     tokenized_data = [doc.split() for doc in group['combined']]
     bm25 = BM25Okapi(tokenized_data, k1=1.5, b=0.5)
     # bm25 = BM25Plus(tokenized_data, k1=1.6, b=0.3, delta=0.7)
     # bm25 = BM25L(tokenized_data, k1=1.8, b=0.3, delta=0.6)
 
+    start = time.time()
     similarity_scores = bm25.get_scores(group['desc_token'].iloc[0].split())
+    end = time.time()
+    duration.append(end - start)
     assert len(similarity_scores) == len(group), "Length of similarity scores does not match length of group"
     
     ## detect whether the values in similarity_scores are negative
@@ -40,6 +45,10 @@ def compute_similarity(args):
         for index, row in group.iterrows():
             f.write(f"{row['cve']},{row['owner']},{row['repo']},{row['commit_id']},{similarity_scores[i]},{row['label']}\n")
             i += 1
+            
+    duration = sum(duration) / len(duration)
+    print(f"Average time to compute similarity scores for {cve}: {duration}")
+    return duration
 
 def process_full_dataframe(df):
     df['desc_token'].fillna(' ', inplace=True)
@@ -50,9 +59,11 @@ def process_full_dataframe(df):
     cve_list = df['cve'].unique()
 
     pool = mp.Pool(mp.cpu_count())
-    list(tqdm(pool.imap_unordered(compute_similarity, [(group, cve) for cve, group in data_cve]), total=len(cve_list)))
+    durations = list(tqdm(pool.imap_unordered(compute_similarity, [(group, cve) for cve, group in data_cve]), total=len(cve_list)))
     pool.close()
     pool.join()
+    avg_duration = sum(durations) / len(durations)
+    print(f"Average time to compute similarity scores: {avg_duration}")
 
 def main():
     similarity_data_file = os.path.join(SAVE_DIR, 'similarity_data_bm25okapi.csv')
